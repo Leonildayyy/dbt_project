965e47a70a390171d48e22340acd3e93b20e722b
YUAN YI <yiyua049@student.liu.se>
2025-08-17 14:15:34 +0000

    analysis_seed

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -21,6 +21,13 @@ orders as (
 
 ),
 
+employees as (
+
+    select
+    *
+    from {{ ref('employees') }}
+
+),
 customer_orders as (
 
     select
@@ -42,13 +49,15 @@ final as (
         customers.customer_id,
         customers.first_name,
         customers.last_name,
+        employees.employee_id is not null as is_employee,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
-
     left join customer_orders using (customer_id)
+    left join employees using (customer_id)
+
 
 )
 
